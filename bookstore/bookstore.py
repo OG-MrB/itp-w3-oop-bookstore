1d2025754da05ac10038cb19f90f80ff3d3f2dae
Eric <efletcher001@gmail.com>
2017-04-07 01:58:13 +0000

    Eric & Jordan OOP Bookstore

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,43 @@
 class Bookstore(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.allbooks = []
+        
+    def get_books(self):
+        return self.allbooks
+        
+    def add_book(self,book):
+        self.allbooks.append(book)
+        
+    def search_books(self, title=None, author=None):
+        book_list = []
+        if title:
+            for book in self.allbooks:
+                if title.lower() in book.title.lower():
+                    book_list.append(book)
+            return book_list
+        if author:
+            for book in self.allbooks:
+                if author ==  book.author:
+                    book_list.append(book)
+            return book_list
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
+# poe = Author('Edgar Allan Poe', 'US')
+# store.search_books(title='raven', author=poe
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.books.append(self)
+    
+        
